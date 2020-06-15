c10a20f7d472e4cb175fcbbd301c492c96a9928c
Eulogio Enamorado Pallares <eulogio-ep@hotmail.com>
2020-06-15 12:58:10 +0200

    Enemy

---
+++ InfinityRunner/Assets/Scripts/Enemy1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody2D rb2D;
+
+    [SerializeField]
+    float enemyVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D.velocity = new Vector2(enemyVelocity, 0);
+    }
+
+}
