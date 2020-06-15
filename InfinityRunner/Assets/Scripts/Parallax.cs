8e212bd4c846c3b64552d95095f1ffcef6e0791d
Eulogio Enamorado Pallares <eulogio-ep@hotmail.com>
2020-06-15 11:06:15 +0200

    Parallax

---
+++ InfinityRunner/Assets/Scripts/Parallax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField]
+    float parallaxVelocity;
+
+    [SerializeField]
+    bool stopParallax;
+
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        MoveParallax();
+    }
+
+    void MoveParallax()
+    {
+        if (stopParallax == false)
+        {
+            GetComponent<Renderer>().material.mainTextureOffset = new Vector2(((Time.time) * parallaxVelocity), 0);
+        }
+    }
+}
